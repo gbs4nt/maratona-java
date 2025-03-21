12171a7ebafa041159e360f5407a12345e060c8a
Gabriel Lima <glima9719@gmail.com>
2025-03-20 23:49:08 -0300

    doc: Adição do ReadMe.md

--- src/introducao/devdojo/maratonajava/ExercicioArrays01.java
+++ src/introducao/devdojo/maratonajava/ExercicioArrays01.java
@@ -4,6 +4,27 @@ public class ExercicioArrays01 {
     public static void main(String[] args) {
         int[][] dias = new int[3][3];
         dias [0][0] = 31; //Forma de atribuir valor à uma array relacionada ao índice 0
-        System.out.println(dias[0][0]);
+        dias [0] [1] = 28;
+        dias [0] [2] = 26;
+
+        dias [1] [0] = 2445;
+        dias [1] [1] = 234;
+        dias [1] [2] = 45;
+        for(int i = 0; i<dias.length;i++){
+            for(int j= 0; j<dias[i].length;j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+        System.out.println("---------");
+        
+        for(int[] arrBase: dias){
+            for (int num: arrBase){
+                System.out.println(num);
+
+            }
+
+        }
+
+
     }
 }
