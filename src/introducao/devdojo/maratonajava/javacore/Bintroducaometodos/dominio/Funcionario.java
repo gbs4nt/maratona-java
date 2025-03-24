998ffa17f78b7e16620aba2eaf923388d4c9716d
Gabriel Lima <glima9719@gmail.com>
2025-03-24 20:13:32 -0300

    feature: Modificador de acesso

--- src/introducao/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/introducao/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package introducao.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media= 0;
     public void imprime(){
         System.out.println(this.nome);
         System.out.println(this.idade);
@@ -14,16 +15,44 @@ public class Funcionario {
             System.out.print(salario + " ");
         }
 
+
     }
     public void imprimeMediaSalarial(){
         if(salarios==null){
             return;
         }
-        double media= 0;
         for(double salario: salarios){
             media += salario;
         }
         media /=salarios.length;
-        System.out.println("\nMedia salarial "+media);}
+        System.out.println("\nMedia salarial "+media);
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
 }
 
---
+++ src/introducao/devdojo/maratonajava/javacore/Bintroducaometodos/tests/PessoaTest01.java
@@ -0,0 +1,15 @@
+package introducao.devdojo.maratonajava.javacore.Bintroducaometodos.tests;
+
+import introducao.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(70);
+        pessoa.imprime();
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+
+    }
+}
