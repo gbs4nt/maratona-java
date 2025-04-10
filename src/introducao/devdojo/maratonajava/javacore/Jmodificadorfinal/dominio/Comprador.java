3df267470ce2cb44396b9991ff51d928a184a31e
unknown <glima9719@gmail.com>
2025-04-10 11:27:10 -0300

    feat: Modificador final: Tipo referência

--- src/introducao/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
+++ src/introducao/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
@@ -1,18 +1,16 @@
 package introducao.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 
-public class Carro {
+public class Comprador {
     private String nome;
-    public final Comprador COMPRADOR = new Comprador();
-
-    public static final double velocidadeLimite = 205;
 
     public void setNome(String nome){
-        this.nome=nome;
+        this.nome = nome;
 
     }
 
     public String getNome(){
         return nome;
+
     }
 
 }
--- src/introducao/devdojo/maratonajava/javacore/Jmodificadorfinal/teste/CarroTeste01.java
+++ src/introducao/devdojo/maratonajava/javacore/Jmodificadorfinal/teste/CarroTeste01.java
@@ -2,9 +2,16 @@ package introducao.devdojo.maratonajava.javacore.Jmodificadorfinal.teste;
 
 import introducao.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
 
+
 public class CarroTeste01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+
+
+
+        carro.COMPRADOR.setNome("Yasmin");
+        System.out.println(carro.COMPRADOR.getNome());
+
 
 
     }
